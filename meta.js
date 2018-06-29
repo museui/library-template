c207df9b66aef8180f91f54250d0dacfb8b1f99b
myronliu347 <myronliu347@gmail.com>
2018-06-29 22:18:08 +0800

    feat: mata

--- meta.js
+++ meta.js
@@ -14,7 +14,7 @@ module.exports = {
       'type': 'string',
       'required': false,
       'message': 'Project description',
-      'default': 'Nuxt.js project'
+      'default': 'Muse-UI plugin'
     },
     author: {
       'type': 'string',
